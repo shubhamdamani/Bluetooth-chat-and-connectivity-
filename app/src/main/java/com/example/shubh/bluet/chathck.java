ce8b56276b4e255f938aec9a179ce838ff981a33
KeshavGithub1998 <keshavdamanikjj@gmail.com>
2019-10-25 22:22:32 +0530

    issue raise

---
+++ app/src/main/java/com/example/shubh/bluet/chathck.java
@@ -0,0 +1,14 @@
+package com.example.shubh.bluet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//this wall is supposed to be chat wall and the activity will be chanegd.
+
+public class chathck {
+
+
+	
+}
+        
+ 
